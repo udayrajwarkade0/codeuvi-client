41c4083ae287967dee1c546e190f22c4d5ce370d
CodeUvi <151745167+udayrajwarkade0@users.noreply.github.com>
2025-08-17 01:32:29 +0530

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -20,7 +20,7 @@ export const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/register`, {
+      const response = await fetch(`http://codeuvi-server.onrender.com/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(user),
@@ -129,3 +129,4 @@ export const Register = () => {
     </section>
   );
 };
+
